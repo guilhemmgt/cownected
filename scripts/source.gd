d090426d10f2fa641e8a50f04e303f0b48a43c9a
Telessia <dunty.telessia@proton.me>
2025-07-19 19:29:02 +0200

    interactions

--- scripts/source.gd
+++ scripts/source.gd
@@ -4,6 +4,23 @@ extends Interactable3D
 @export var voltage: int = 1
 @export var reach: float = 5.0
 var active: bool = true
+var switch: Switch
 
-func _on_closest(interactor: Interactor3D) -> void:
-	print("meuh meuh")
+func _ready():
+	collision_layer = 2
+	connect("closest", _on_closest)
+	connect("not_closest", _on_not_closest)
+	connect("interacted", _on_interacted)
+
+func _on_closest(interactor: CowInteractor):
+	pass
+	
+func _on_not_closest(interactor: CowInteractor):
+	pass
+	
+func _on_interacted(interactor: CowInteractor):
+	interactor.linked_source = self
+	
+	if switch != null:
+		switch.remove_source(self)
+		switch = null
--- scripts/target.gd
+++ scripts/target.gd
@@ -1,3 +1,10 @@
 class_name Target
-
 extends Node3D
+
+func activate():
+	pass
+	# TO OVERWRITE
+	
+func deactivate():
+	pass
+	# TO OVERWRITE
